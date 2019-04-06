e6b1f41310b9258fd50cbba81f0fceeee95bff42
ChoiBU <cbw0916@google.com>
2019-04-06 23:58:36 +0900

    game component

--- src/component/Game/index.js
+++ src/component/Game/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import * as jobTypes from '../App/const/JobTypes';
+import * as TimeStatus from './const/TimeStatus';
 
 class Game extends Component {
 	state = {
 		date : 1,
+		dayStatus : TimeStatus.DAYTIME,
 		players : this.props.players,
 	}
 
